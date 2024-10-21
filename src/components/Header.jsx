84122342a0e2b86c2c6a61e4f1789cbbe2df1d64
Daniela Bastias <danybastias@outlook.com>
2024-10-21 10:54:49 -0300

    Ultimando detalles

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -19,7 +19,6 @@ function Header() {
           <li><a href="#about">Nosotros</a></li>
           <li><a href="#services">Servicios</a></li>
           <li><a href="#clients">Clientes</a></li>
-          <li><a href="#development">Desarrollo Web</a></li>
           <li><a href="#contact">Contacto</a></li>
         </ul>
       </nav>
