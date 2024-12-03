33ef4a87281fe5240a35b41d724e30fd1a2ee0aa
Daniela Bastias <danybastias@outlook.com>
2024-12-03 13:36:41 -0300

    Arreglando el deploy

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: 'https://github.com/Nannys-cmd/wtics', // Cambia 'wtics' por el nombre de tu repositorio
+  base: 'https://github.com/Nannys-cmd/wtics.git', // Cambia 'wtics' por el nombre de tu repositorio
 });
