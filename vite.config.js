5a992a2f96cece6546c5e4ef6cf1bf3960fa2987
Daniela Bastias <danybastias@outlook.com>
2025-02-06 18:15:04 -0300

    Modificando archivo vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: 'https://github.com/Nannys-cmd/wtics.git', // Cambia 'wtics' por el nombre de tu repositorio
+  base: 'https://nannys-cmd.github.io/wtics/', // Cambia 'wtics' por el nombre de tu repositorio
 });
