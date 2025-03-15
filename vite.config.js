6780d178798098044e29191deace6bd92dfac1e9
Daniela Bastias <bastiasdany.84@gmail.com>
2025-03-15 17:51:25 -0300

    Deploy realizado con éxito

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/wtics/', // Usar solo la ruta relativa de tu repositorio
+  base: '/wtics/', // Asegúrate de que coincida con el nombre del repositorio
 });
